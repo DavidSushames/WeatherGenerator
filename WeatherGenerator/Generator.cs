52334dbf184e589c75805c388847c5d4eb922aea
Joseph Wijnstok <wijnstjose@ecs.vuw.ac.nz>
2025-11-02 12:42:50 +1300

    created dice model begun work on main generator class

---
+++ WeatherGenerator/Generator.cs
@@ -0,0 +1,13 @@
+﻿using Weather_Generation.WeatherGenerator.Model.Enums;
+
+namespace Weather_Generation.WeatherGenerator
+{
+    public class Generator
+    {
+        private Season season;
+        private Biome biome;
+        private bool Midwinter = false;
+
+
+    }
+}
---
+++ WeatherGenerator/Model/DiceModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Weather_Generation.WeatherGenerator.Model
+{
+    public class DiceModel
+    {
+        private Random rng;
+
+        public int WindDice { get; private set; }
+        public int PrecipDice { get; private set; }
+        public int TempretureDice { get; private set; }
+
+        public DiceModel()
+        {
+            rng = new Random(Guid.NewGuid().GetHashCode());
+            WindDice = rng.Next(1, 7);
+            PrecipDice = rng.Next(1, 7);
+            TempretureDice = rng.Next(1, 7);
+        }
+
+        public DiceModel(int seedValue)
+        {
+            rng = new Random(seedValue);
+            WindDice = rng.Next(1, 7);
+            PrecipDice = rng.Next(1, 7);
+            TempretureDice = rng.Next(1, 7);
+        }
+
+        public void RerollWind()
+        {
+            rng = new Random(Guid.NewGuid().GetHashCode());
+            WindDice = rng.Next(1, 7);
+        }
+
+        public void RerollWind(int seedValue)
+        {
+            rng = new Random(seedValue);
+            WindDice = rng.Next(1, 7);
+        }
+
+        public void RerollPrecip()
+        {
+            rng = new Random(Guid.NewGuid().GetHashCode());
+            PrecipDice = rng.Next(1, 7);
+        }
+
+        public void RerollPrecip(int seedValue)
+        {
+            rng = new Random(seedValue);
+            PrecipDice = rng.Next(1, 7);
+        }
+
+        public void RerollTemp()
+        {
+            rng = new Random(Guid.NewGuid().GetHashCode());
+            TempretureDice = rng.Next(1, 7);
+        }
+
+        public void RerollTemp(int seedValue)
+        {
+            rng = new Random(seedValue);
+            TempretureDice = rng.Next(1, 7);
+        }
+
+    }
+}
