c6a26626b202f345d6f1522f03afc68365f5a0ef
Joseph Wijnstok <wijnstjose@ecs.vuw.ac.nz>
2025-11-02 13:24:59 +1300

    Created Weather model

--- WeatherGenerator/Model/Weather.cs
+++ WeatherGenerator/Model/Weather.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Weather_Generation.WeatherGenerator.Model.Enums;
 
 namespace Weather_Generation.WeatherGenerator.Model
 {
-    internal class Weather
+    public class Weather
     {
+
+        public WeatherWindEffects WindEffect { get; private set; }
+        public WeatherPrecipEffects PrecipEffect { get; private set; }
+        public WeatherTempEffects TempEffect { get; private set; }
+
+        public Weather(WeatherWindEffects wind, WeatherTempEffects temp, WeatherPrecipEffects precip)
+        {
+            WindEffect = wind;
+            PrecipEffect = precip;
+            TempEffect = temp;
+        }
+
     }
 }
